735adffb7a9d95302f475d8cbcc2df09a1688d65
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-07-25 18:34:42 -0500

    feat: Add validation to the endpoints globally
    - Add `class-validator` and `class-transformer`.
    - Use some decorator to validate DTOs.

--- src/videos/dto/create-video.dto.ts
+++ src/videos/dto/create-video.dto.ts
@@ -1,1 +1,15 @@
-export class CreateVideoDto {}
+import { IsNotEmpty, IsUrl, Length } from 'class-validator';
+
+export class CreateVideoDto {
+  @Length(3, 50)
+  @IsNotEmpty()
+  title: string;
+
+  @Length(5, 50)
+  @IsNotEmpty()
+  description: string;
+
+  @IsUrl()
+  @IsNotEmpty()
+  src: string;
+}
