425f72fbd5cf1c1e18a2ebded0f9d73381fb63f9
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-07-26 12:19:15 -0500

    feat: Enable versioning

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -15,6 +15,13 @@ async function bootstrap() {
   console.log({ port: process.env.PORT });
 
   app.useGlobalPipes(new ValidationPipe());
+
+  // Versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Courses API Documentation')
     .addBearerAuth()
