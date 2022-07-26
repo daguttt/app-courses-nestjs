2baa27ad0699737abe66ca4d9cc2c6ee01949bc0
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-07-26 12:34:02 -0500

    feat: Serve an `index.html` as a root route
    - Add Serve Static package.
    - Commit when Serve Static package was added:
     425f72fbd5cf1c1e18a2ebded0f9d73381fb63f9

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,16 @@ import { AwardsModule } from './awards/awards.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
     CoursesModule,
     AuthModule,
