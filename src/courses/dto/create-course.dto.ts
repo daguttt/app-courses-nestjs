98f7669d8f896f03cf216b14c07c467179461a1a
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-07-26 11:01:21 -0500

    feat: Add CreateCourseDto properties to the documentation

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -1,6 +1,24 @@
-import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsUrl, Length } from 'class-validator';
 
 export class CreateCourseDto {
+  @ApiProperty({
+    minimum: 1,
+  })
+  @Length(1, 50)
   @IsNotEmpty()
   title: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  price: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  description: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsUrl()
+  cover: string;
 }
