c0f25a9ee908263c77d41cd616af1043fb1c761d
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-07-28 18:44:01 -0500

    Class 13: Pipes

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -8,11 +8,13 @@ import {
   Delete,
   HttpException,
   HttpStatus,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { CoursesService } from './courses.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
+import { SlugPipe } from './pipes/slug.pipe';
 @ApiBearerAuth()
 @ApiTags('courses')
 @Controller('courses')
@@ -35,9 +37,28 @@ export class CoursesController {
     return this.coursesService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.coursesService.findOne(+id);
+  // @Get(':id')
+  // findOne(@Param('id', ParseIntPipe) id: number) {
+  //   return this.coursesService.findOne(id);
+  // }
+  // @Get(':id')
+  // findOne(
+  //   @Param(
+  //     'id',
+  //     new ParseIntPipe({
+  //       errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE,
+  //     }),
+  //   )
+  //   id: number,
+  // ) {
+  //   return this.coursesService.findOne(id);
+  @Get(':title')
+  findOne(
+    @Param('title', new SlugPipe())
+    title: string,
+  ) {
+    console.log('Search by title: ...', { title });
+    return this.coursesService.findOne(2);
   }
 
   @Patch(':id')
